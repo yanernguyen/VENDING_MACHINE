3e73b2010265accf8de87814385db2da055b4272
yanernguyen <nhinty23411@st.uel.edu.vn>
2025-03-21 19:50:36 +0700

    Chỉnh sửa Product

--- CProduct.py
+++ CProduct.py
@@ -1,24 +1,35 @@
-
-
 import json
-from typing import List, Dict
+from typing import Optional
 
 
 class Product:
-    def __init__(self, name: str, category: str, price: float, stock: int):
+    _next_id = 1
+
+    def __init__(self, name: str, category: str, price: float,
+                 stock: int, image: str = None, id: str = None):
+
         self.name = name
         self.category = category
         self.price = price
         self.stock = stock
-
-    def __str__(self):
-        return f"{self.name} (${self.price}) - {self.stock} in stock"
+        self.image = image
+        self.id = id
 
     def to_dict(self):
         return {
+            "id": self.id,
             "name": self.name,
-            "category": self.category,
             "price": self.price,
             "stock": self.stock,
+            "category": self.category,
+            "image": self.image,
         }
+
+    def is_valid(self) -> bool:
+        return (
+                isinstance(self.name, str) and self.name.strip() != "" and
+                isinstance(self.category, str) and self.category.strip() != "" and
+                self.price > 0 and
+                self.stock >= 0
+        )
 
